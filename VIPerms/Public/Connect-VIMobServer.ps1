62d10845ba279e324ea2589e5862bdfa5a979a66
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 09:01:13 +0200

    Refactored
    + OUTPUTS in comment-help
    + BEGIN, PROCESS, and END blocks for easier logic flow
    + Comments
    ~ Fixed SkipCertificateCheck in $Script:VIPerms not working
    - Whitespace

--- VIPerms/Public/Connect-VIMobServer.ps1
+++ VIPerms/Public/Connect-VIMobServer.ps1
@@ -1,71 +1,97 @@
 function Connect-VIMobServer {
     <#
-    .SYNOPSIS
-    Connect to the vSphere Managed Object Browser (MOB).
-    
-    .DESCRIPTION
-    This function will first test the connection to the specified vCenter server MOB. If successful
-    the Uri and PSCredentials object are stored in the global variable $Global:VIPerms for use
-    with other functions in this module. 
-    
-    .PARAMETER Server
-    Specify the name of a vCenter server.
-    
-    .PARAMETER Credential
-    Specify the credentials to use to authenticate against the MOB.
-    This is usually administrator@vsphere.local
-    
-    .PARAMETER SkipCertificateCheck
-    Skip certificate verification.
-    
-    .EXAMPLE
-    Connect-VIMobServer -Server "vcenter.example.com"
+        .SYNOPSIS
+            Connect to the vSphere Managed Object Browser (MOB).
+
+        .DESCRIPTION
+            This function will first test the connection to the specified vCenter server MOB. If successful
+            the Uri and PSCredentials object are stored in the global variable $Global:VIPerms for use
+            with other functions in this module.
+
+        .PARAMETER Server
+            Specify the name of a vCenter server.
+
+        .PARAMETER Credential
+            Specify the credentials to use to authenticate against the MOB.
+            This is usually administrator@vsphere.local
+
+        .PARAMETER SkipCertificateCheck
+            Skip certificate verification.
+
+        .EXAMPLE
+            Connect-VIMobServer -Server "vcenter.example.com"
+
+        .OUTPUTS
+            [PSCustomObject]@{
+                Server = Server
+                User   = UserName
+            }
     #>
 
 
     param (
         [Parameter(
-            Position = 0,
+            Position  = 0,
             Mandatory = $true
         )]
-        [string] $Server,
+        [string]$Server,
 
         [Parameter(
-            Position = 1,
+            Position  = 1,
             Mandatory = $true
         )]
-        [PSCredential] $Credential,
+        [PSCredential]$Credential,
 
         [Parameter(
-            Position = 2,
+            Position  = 2,
             Mandatory = $false
         )]
-        [Switch] $SkipCertificateCheck
+        [Switch]$SkipCertificateCheck
     )
 
-    try {
-        $ProPref = $ProgressPreference
+    BEGIN {
+        $ProPref            = $ProgressPreference
         $ProgressPreference = "SilentlyContinue"
-        if ($SkipCertificateCheck) {
-            Set-CertPolicy -SkipCertificateCheck
-        }
-        $Global:VIPerms = @{
-            Server = $Server
-            Credential = $Credential
-            SkipCertificateCheck = $true
-        }
-        Invoke-Login -Server $Server -Credential $Credential
-        [PSCustomObject] @{
-            Server = $Server
-            User = $Credential.GetNetworkCredential().UserName
-        }
-        Invoke-Logoff
-        if ($SkipCertificateCheck) {
-            Set-CertPolicy -ResetToDefault
-        }
+    } #BEGIN
+
+    PROCESS {
+
+        try {
+
+            if ($SkipCertificateCheck) {
+                Set-CertPolicy -SkipCertificateCheck
+            } #if
+            
+            $Script:VIPerms = @{
+                Server               = $Server
+                Credential           = $Credential
+                SkipCertificateCheck = $SkipCertificateCheck.IsPresent
+            } #$Script:VIPerms
+
+            Invoke-Login -Server $Server -Credential $Credential
+
+            Invoke-Logoff
+
+            if ($SkipCertificateCheck) {
+                Set-CertPolicy -ResetToDefault
+            } #if
+
+            return ([PSCustomObject]@{
+                Server = $Server
+                User   = $Credential.GetNetworkCredential().UserName
+            })
+
+        } #try
+        catch {
+            $Err = $_
+            throw $Err
+        } #catch
+
+    } #PROCESS
+
+    END {
         $ProgressPreference = $ProPref
-    } catch {
-        $Err = $_
-        throw $Err
-    }
-}
+    } #END
+
+
+} #function Connect-VIMobServer
