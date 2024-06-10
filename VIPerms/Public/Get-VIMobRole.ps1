8cd74e8651e24b61c114679e09de91ab39ca38ee
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 12:25:46 +0200

    Refactored
    + Comments
    + BEGIN, PROCESS, and END block for easier logic
    + Text alignment
    + ProgressPreference return in catch block
    + Types
    + Explicit return/write-output
    - Mandatory = $False in SkipCertificateCheck parameter block - Unecessary if false, default is false.

--- VIPerms/Public/Get-VIMobRole.ps1
+++ VIPerms/Public/Get-VIMobRole.ps1
@@ -1,69 +1,88 @@
 function Get-VIMobRole {
     <#
-    .SYNOPSIS
-    Get roles via the Managed Object Browser (MOB)
-    
-    .DESCRIPTION
-    Retrieve all available roles from the vCenter MOB. This is not the most effective way of retrieving this
-    information. If you are using PowerCLI you should use the built in Get-VIRole CmdLet instead.
-    
-    .PARAMETER SkipCertificateCheck
-    Skip certificate verification.
-    
-    .EXAMPLE
-    Get-VIMobRole
-    #>
+        .SYNOPSIS
+            Get roles via the Managed Object Browser (MOB)
 
+        .DESCRIPTION
+            Retrieve all available roles from the vCenter MOB. This is not the most effective way of retrieving this
+            information. If you are using PowerCLI you should use the built in Get-VIRole CmdLet instead.
+
+        .PARAMETER SkipCertificateCheck
+            Skip certificate verification.
+
+        .EXAMPLE
+            Get-VIMobRole
+    #>
 
     param(
         [Parameter(
-            Position = 0,
-            Mandatory = $false
+            Position = 0
         )]
         [Switch] $SkipCertificateCheck
     )
-    
-    try {
-        $ProPref = $ProgressPreference
-        $ProgressPreference = "SilentlyContinue"
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -SkipCertificateCheck
-        }
-        Invoke-Login
-        $Uri = ("https://$($Global:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&" +
-                "method=AuthorizationService.GetRoles")
-        $Body = "vmware-session-nonce=$($Global:VIPerms.SessionNonce)"
-        $Params = @{
-            Uri = $Uri
-            WebSession = $Global:VIPerms.WebSession
-            Method = "POST"
-            Body = $Body
-        }
-        $Res = Invoke-WebRequest @Params
-        $Table = $Res.ParsedHtml.body.getElementsByTagName("table")[3]
-        $Td = $Table.getElementsByTagName("tr")[4].getElementsByTagName("td")[2]
-        $Li = $Td.getElementsByTagName("ul")[0].getElementsByTagName("li")
-        
-        foreach ($Item in $Li) {
-            if ($Item.innerHTML.StartsWith("<TABLE")) {
-                $Description = $Item.getElementsByTagName("tr")[1].getElementsByTagName("td")[2].innerText
-                $Id = $Item.getElementsByTagName("tr")[4].getElementsByTagName("td")[2].innerText
-                $Name = $Item.getElementsByTagName("tr")[5].getElementsByTagName("td")[2].innerText
 
-                [PSCustomObject] @{
-                    Name = $Name
-                    Description = $Description
-                    Id = $Id
-                }
+    BEGIN {
+
+        $ProPref            = $ProgressPreference
+        $ProgressPreference = "SilentlyContinue"
+
+    } #BEGIN
+    PROCESS {
+
+        try {
+
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -SkipCertificateCheck
             }
-        }
-        Invoke-Logoff
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -ResetToDefault
-        }
+            Invoke-Login
+            [String]$Uri  = "https://$($Script:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&method=AuthorizationService.GetRoles"
+            [String]$Body = "vmware-session-nonce=$($Script:VIPerms.SessionNonce)"
+            $Params = @{
+                Uri        = $Uri
+                WebSession = $Script:VIPerms.WebSession
+                Method     = "POST"
+                Body       = $Body
+            }
+            $Res   = Invoke-WebRequest @Params
+            $Table = $Res.ParsedHtml.body.getElementsByTagName("table")[3]
+            $Td    = $Table.getElementsByTagName("tr")[4].getElementsByTagName("td")[2]
+            $Li    = $Td.getElementsByTagName("ul")[0].getElementsByTagName("li")
+
+            foreach ($Item in $Li) {
+
+                if ($Item.innerHTML.StartsWith("<TABLE")) {
+                    $Description = $Item.getElementsByTagName("tr")[1].getElementsByTagName("td")[2].innerText
+                    $Id          = $Item.getElementsByTagName("tr")[4].getElementsByTagName("td")[2].innerText
+                    $Name        = $Item.getElementsByTagName("tr")[5].getElementsByTagName("td")[2].innerText
+
+                    Write-Output (@{
+                        Name        = $Name
+                        Description = $Description
+                        Id          = $Id
+                    })
+                } #if
+
+            } #foreach
+
+            Invoke-Logoff
+
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -ResetToDefault
+            } #if
+
+        } #try
+        catch {
+
+            $Err = $_
+            $ProgressPreference = $ProPref
+            throw $Err
+
+        } #catch
+    } #PROCESS
+    END {
+
         $ProgressPreference = $ProPref
-    } catch {
-        $Err = $_
-        throw $Err
-    }
-}
+
+    } #END
+
+} #function Get-VIMobRole
