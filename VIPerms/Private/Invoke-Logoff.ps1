366743b1f2e403e5cab25b2019b5e02c19b07d5c
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 12:49:23 +0200

    Voided unused response

--- VIPerms/Private/Invoke-Logoff.ps1
+++ VIPerms/Private/Invoke-Logoff.ps1
@@ -3,25 +3,29 @@ function Invoke-Logoff {
     .SYNOPSIS
     Logout of the authenticated vCenter MOB web session and clear up the Script variable VIPerms.
     #>
-    
+
     BEGIN {
+
         $ProPref            = $ProgressPreference
         $ProgressPreference = "SilentlyContinue"
+
     } #BEGIN
 
     PROCESS {
+
         try {
             [String]$Uri = "https://$($Script:VIPerms.Server)/invsvc/mob3/logout"
-            $Res         = Invoke-WebRequest -Uri $Uri -WebSession $Script:VIPerms.WebSession -Method GET
+            [Void](Invoke-WebRequest -Uri $Uri -WebSession $Script:VIPerms.WebSession -Method GET)
             $Script::VIPerms.WebSession   = $null
             $Script::VIPerms.SessionNonce = $null
-    
+
         } #try
         catch {
             $Err = $_
             $ProgressPreference = $ProPref
             throw $Err
         } #catch
+
     } #PROCESS
 
     END {
