447dba943241ed6da04cdc62b00fa68e65faffdf
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 08:42:56 +0200

    +Refactored
    + BEGIN, PROCESS, END blocks for easier logic flow
    + Types where it's obvious
    ~ Global to Script scope for spanning variables

--- VIPerms/Private/Invoke-Login.ps1
+++ VIPerms/Private/Invoke-Login.ps1
@@ -8,38 +8,51 @@ function Invoke-Login {
     VIPerms hashtable along with the session variable from the web request.   
     #>
 
-    try {
-        $ProPref = $ProgressPreference
+    BEGIN {
+        $ProPref            = $ProgressPreference
         $ProgressPreference = "SilentlyContinue"
-        if (!($Global:VIPerms.Server) -or ([String]::IsNullOrEmpty($Global:VIPerms.Server)) -or
-            !($Global:VIPerms.Credential) -or ([String]::IsNullOrEmpty($Global:VIPerms.Credential))) {
-            throw "Please authenticate using Connect-VIMobServer first!"
-        }
-        $Uri = ("https://$($Global:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&" +
-                "method=AuthorizationService.GetRoles")
-        
-        # Initial login to vSphere MOB to store session variable
-        $Params = @{
-            Uri = $Uri
-            SessionVariable = "MobSession"
-            Credential = $Global:VIPerms.Credential
-            Method = "GET"
-        }
-        $Res = Invoke-WebRequest @Params
-        
-        # Extract hidden vmware-session-nonce which must be included in future requests to prevent CSRF error
-        # Credit to https://blog.netnerds.net/2013/07/use-powershell-to-keep-a-cookiejar-and-post-to-a-web-form/ for
-        # parsing vmware-session-nonce via Powershell
-        if ($Res.StatusCode -eq 200) {
-            $null = $Res -match 'name="vmware-session-nonce" type="hidden" value="?([^\s^"]+)"'
-            $Global:VIPerms.SessionNonce = $Matches[1]
-            $Global:VIPerms.WebSession = $MobSession
-        } else {
-            throw "Failed to login to vSphere MOB"
-        }
+    } #BEGIN
+
+    PROCESS {
+    
+        try {
+            if (!($Script:VIPerms.Server) -or ([String]::IsNullOrEmpty($Script:VIPerms.Server)) -or
+                !($Script:VIPerms.Credential) -or ([String]::IsNullOrEmpty($Script:VIPerms.Credential))) {
+                throw "Please authenticate using Connect-VIMobServer first!"
+            }
+            [String]$Uri = "https://$($Script:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&method=AuthorizationService.GetRoles"
+            
+            # Initial login to vSphere MOB to store session variable
+            [Hashtable]$Params = @{
+                Uri             = $Uri
+                SessionVariable = "MobSession"
+                Credential      = $Script:VIPerms.Credential
+                Method          = "GET"
+            }
+            $Res = Invoke-WebRequest @Params
+            
+            # Extract hidden vmware-session-nonce which must be included in future requests to prevent CSRF error
+            # Credit to https://blog.netnerds.net/2013/07/use-powershell-to-keep-a-cookiejar-and-post-to-a-web-form/ for
+            # parsing vmware-session-nonce via Powershell
+            if ($Res.StatusCode -eq 200) {
+                [void]($Res -match 'name="vmware-session-nonce" type="hidden" value="?([^\s^"]+)"')
+                $Script:VIPerms.SessionNonce = $Matches[1]
+                $Script:VIPerms.WebSession   = $MobSession
+            } #if 
+            else {
+                throw "Failed to login to vSphere MOB"
+            } #else
+
+        } #try
+        catch {
+            $Err = $_
+            throw $Err
+        } #catch
+    } #PROCESS
+
+    END {
         $ProgressPreference = $ProPref
-    } catch {
-        $Err = $_
-        throw $Err
-    }
-}
+    } #EMD
+
+
+} #function Invoke-Login
