b064b686305e8cf4623c4c3fd2320ac78726e55e
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 12:31:13 +0200

    Refactored
    + Comments
    + BEGIN, PROCESS, and END block for easier logic
    + Text alignment
    + ProgressPreference return in catch block
    + Types
    + Explicit return/write-output
    - Mandatory = $False in SkipCertificateCheck parameter block - Unecessary if false, default is false.
    - Unnecessary switch case

--- VIPerms/Public/Remove-VIGlobalPermission.ps1
+++ VIPerms/Public/Remove-VIGlobalPermission.ps1
@@ -1,78 +1,93 @@
 function Remove-VIGlobalPermission {
     <#
-    .SYNOPSIS
-    Delete a global permission for a specific user/group.
-    
-    .DESCRIPTION
-    Delete a global permission for a specific user/group.
-    
-    .PARAMETER Name
-    Specify the name of user or group including the domain.
-    
-    .PARAMETER IsGroup
-    Specify whether the target is a group object or not.
-    
-    .PARAMETER SkipCertificateCheck
-    Skip certificate verification.
-    
-    .EXAMPLE
-    Remove-VIGlobalPermission -Name "VSPHERE.LOCAL\Administrator"
+        .SYNOPSIS
+            Delete a global permission for a specific user/group.
 
-    .EXAMPLE
-    Remove-VIGlobalPermission -Name "VSPHERE.LOCAL\group-of-users" -IsGroup
+        .DESCRIPTION
+            Delete a global permission for a specific user/group.
+
+        .PARAMETER Name
+            Specify the name of user or group including the domain.
+
+        .PARAMETER IsGroup
+            Specify whether the target is a group object or not.
+
+        .PARAMETER SkipCertificateCheck
+            Skip certificate verification.
+
+        .EXAMPLE
+            Remove-VIGlobalPermission -Name "VSPHERE.LOCAL\Administrator"
+
+        .EXAMPLE
+            Remove-VIGlobalPermission -Name "VSPHERE.LOCAL\group-of-users" -IsGroup
     #>
-
 
     param (
         [Parameter(
             Position = 0,
             Mandatory = $true
         )]
-        [String] $Name,
+        [String]$Name,
 
         [Parameter(
-            Position = 1,
-            Mandatory = $false
+            Position = 1
         )]
-        [Switch] $IsGroup,
+        [Switch]$IsGroup,
 
         [Parameter(
-            Position = 2,
-            Mandatory = $false
+            Position = 2
         )]
-        [Switch] $SkipCertificateCheck
+        [Switch]$SkipCertificateCheck
     )
 
-    try {
+    BEGIN {
         $ProPref = $ProgressPreference
         $ProgressPreference = "SilentlyContinue"
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -SkipCertificateCheck
-        }
-        Invoke-Login
-        $Uri = ("https://$($Global:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&" +
-                "method=AuthorizationService.RemoveGlobalAccess")
-        $Group = switch ($IsGroup) {
-            $true {"true"}
-            $false {"false"}
-        }
-        $Body = ("vmware-session-nonce=$($Global:VIPerms.SessionNonce)&" +
-                 "principals=%3Cprincipals%3E%0D%0A+++%3Cname%3E$([Uri]::EscapeUriString($Name))" +
-                 "%3C%2Fname%3E%0D%0A+++%3Cgroup%3E$Group%3C%2Fgroup%3E%0D%0A%3C%2Fprincipals%3E")
-        $Params = @{
-            Uri = $Uri
-            WebSession = $Global:VIPerms.WebSession
-            Method = "POST"
-            Body = $Body
-        }
-        $Res = Invoke-WebRequest @Params
-        Invoke-Logoff
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -ResetToDefault
-        }
+    } #BEGIN
+
+    PROCESS {
+
+        try {
+
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -SkipCertificateCheck
+            } #if
+
+            Invoke-Login
+
+            [String]$Uri = "https://$($Script:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&method=AuthorizationService.RemoveGlobalAccess"
+
+            $Body = (
+                "vmware-session-nonce=$($Script:VIPerms.SessionNonce)&" +
+                "principals=%3Cprincipals%3E%0D%0A+++%3Cname%3E$([Uri]::EscapeUriString($Name))" +
+                "%3C%2Fname%3E%0D%0A+++%3Cgroup%3E$($IsGroup.IsPresent)%3C%2Fgroup%3E%0D%0A%3C%2Fprincipals%3E"
+            ) #Body
+
+            $Params = @{
+                Uri        = $Uri
+                WebSession = $Script:VIPerms.WebSession
+                Method     = "POST"
+                Body       = $Body
+            } #Params
+
+            [Void](Invoke-WebRequest @Params)
+
+            Invoke-Logoff
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -ResetToDefault
+            } #if
+
+        } #try
+        catch {
+            $Err = $_
+            $ProgressPreference = $ProPref
+            throw $Err
+        } #catch
+
+    } #PROCESS
+
+    END {
         $ProgressPreference = $ProPref
-    } catch {
-        $Err = $_
-        throw $Err
-    }
-}
+    } #END
+
+} #function Remove-VIGlobalPermission
