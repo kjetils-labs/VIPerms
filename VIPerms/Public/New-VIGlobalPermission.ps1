e82a5e80ee81ac46717450f91741a937b004b9ab
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-06-10 12:41:43 +0200

    Refactored
    + Comments
    + BEGIN, PROCESS, and END block for easier logic
    + Text alignment
    + ProgressPreference return in catch block
    + Types
    + Void unused response
    - Mandatory = $False in SkipCertificateCheck parameter block - Unecessary if false, default is false.
    - Unnecessary switch cases

--- VIPerms/Public/New-VIGlobalPermission.ps1
+++ VIPerms/Public/New-VIGlobalPermission.ps1
@@ -1,34 +1,37 @@
 function New-VIGlobalPermission {
     <#
-    .SYNOPSIS
-    Add a global permission for a user/group.
-    
-    .DESCRIPTION
-    Creates a global permission assigning either a user or group to a specific role.
-    
-    .PARAMETER Name
-    Specify the name of user or group including the domain.
-    
-    .PARAMETER IsGroup
-    Specify whether the target is a group object or not.
-    
-    .PARAMETER RoleId
-    Specify the identifier for the specific role to assign to the global permission.
-    
-    .PARAMETER Propagate
-    Specify whether the permission should propagate to all children objects or not.
-    
-    .PARAMETER SkipCertificateCheck
-    Skip certificate verification.
-    
-    .EXAMPLE
-    New-VIGlobalPermission -Name "VSPHERE.LOCAL\joe-bloggs" -RoleId -1
+        .SYNOPSIS
+            Add a global permission for a user/group.
 
-    .EXAMPLE
-    New-VIGlobalPermission -Name "VSPHERE.LOCAL\group-of-users" -IsGroup -RoleId -1
+        .DESCRIPTION
+            Creates a global permission assigning either a user or group to a specific role.
 
-    .EXAMPLE
-    New-VIGlobalPermission -Name "VSPHERE.LOCAL\joe-bloggs" -RoleId -1 -Propagate:$false
+        .PARAMETER Name
+            Specify the name of user or group including the domain.
+
+        .PARAMETER IsGroup
+            Specify whether the target is a group object or not.
+
+        .PARAMETER RoleId
+            Specify the identifier for the specific role to assign to the global permission.
+
+        .PARAMETER Propagate
+            Specify whether the permission should propagate to all children objects or not.
+
+        .PARAMETER SkipCertificateCheck
+            Skip certificate verification.
+
+        .EXAMPLE
+            New-VIGlobalPermission -Name "VSPHERE.LOCAL\joe-bloggs" -RoleId -1
+
+        .EXAMPLE
+            New-VIGlobalPermission -Name "VSPHERE.LOCAL\group-of-users" -IsGroup -RoleId -1
+
+        .EXAMPLE
+            New-VIGlobalPermission -Name "VSPHERE.LOCAL\joe-bloggs" -RoleId -1 -Propagate:$false
+
+        .OUTPUTS
+            None
     #>
 
 
@@ -37,70 +40,82 @@ function New-VIGlobalPermission {
             Position = 0,
             Mandatory = $true
         )]
-        [String] $Name,
+        [String]$Name,
 
         [Parameter(
-            Position = 1,
-            Mandatory = $false
+            Position = 1
         )]
-        [Switch] $IsGroup,
+        [Switch]$IsGroup,
 
         [Parameter(
-            Position = 2,
-            Mandatory = $false
+            Position = 2
         )]
-        [String] $RoleId,
+        [String]$RoleId,
 
         [Parameter(
-            Position = 3,
-            Mandatory = $false
+            Position = 3
         )]
-        [Switch] $Propagate = [Switch]::Present,
+        [Switch]$Propagate = [Switch]::Present,
 
         [Parameter(
-            Position = 4,
-            Mandatory = $false
+            Position = 4
         )]
-        [Switch] $SkipCertificateCheck
+        [Switch]$SkipCertificateCheck
     )
-    
-    try {
-        $ProPref = $ProgressPreference
+
+    BEGIN {
+
+        $ProPref            = $ProgressPreference
         $ProgressPreference = "SilentlyContinue"
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -SkipCertificateCheck
-        }
-        Invoke-Login
-        $Uri = ("https://$($Global:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&" +
-                "method=AuthorizationService.AddGlobalAccessControlList")
-        $Group = switch ($IsGroup) {
-            $true {"true"}
-            $false {"false"}
-        }
-        $Prop = switch ($Propagate) {
-            $true {"true"}
-            $false {"false"}
-        }
-        $Body = ("vmware-session-nonce=$($Global:VIPerms.SessionNonce)&" +
-                 "permissions=%3Cpermissions%3E%0D%0A+++%3Cprincipal%3E%0D%0A++++++" +
-                 "%3Cname%3E$([Uri]::EscapeUriString($Name))%3C%2Fname%3E" +
-                 "%0D%0A++++++%3Cgroup%3E$Group%3C%2Fgroup%3E%0D%0A+++%3C%2Fprincipal%3E%0D%0A+++" +
-                 "%3Croles%3E$RoleId%3C%2Froles%3E%0D%0A+++" +
-                 "%3Cpropagate%3E$Prop%3C%2Fpropagate%3E%0D%0A%3C%2Fpermissions%3E")
-        $Params = @{
-            Uri = $Uri
-            WebSession = $Global:VIPerms.WebSession
-            Method = "POST"
-            Body = $Body
-        }
-        $Res = Invoke-WebRequest @Params
-        Invoke-Logoff
-        if ($SkipCertificateCheck -or $Global:VIPerms.SkipCertificateCheck) {
-            Set-CertPolicy -ResetToDefault
-        }
+
+    } #BEGIN
+
+    PROCESS {
+
+        try {
+
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -SkipCertificateCheck
+            } #if
+
+            Invoke-Login
+            [String]$Uri = "https://$($Script:VIPerms.Server)/invsvc/mob3/?moid=authorizationService&method=AuthorizationService.AddGlobalAccessControlList"
+
+            [String]$Body = (
+                "vmware-session-nonce=$($Script:VIPerms.SessionNonce)&" +
+                "permissions=%3Cpermissions%3E%0D%0A+++%3Cprincipal%3E%0D%0A++++++" +
+                "%3Cname%3E$([Uri]::EscapeUriString($Name))%3C%2Fname%3E" +
+                "%0D%0A++++++%3Cgroup%3E$($IsGroup.IsPresent)%3C%2Fgroup%3E%0D%0A+++%3C%2Fprincipal%3E%0D%0A+++" +
+                "%3Croles%3E$RoleId%3C%2Froles%3E%0D%0A+++" +
+                "%3Cpropagate%3E$($Propagate.IsPresent)%3C%2Fpropagate%3E%0D%0A%3C%2Fpermissions%3E"
+            ) #body
+
+            [Hashtable]$Params = @{
+                Uri        = $Uri
+                WebSession = $Script:VIPerms.WebSession
+                Method     = "POST"
+                Body       = $Body
+            } #param
+
+            [Void](Invoke-WebRequest @Params)
+
+            Invoke-Logoff
+
+            if ($SkipCertificateCheck -or $Script:VIPerms.SkipCertificateCheck) {
+                Set-CertPolicy -ResetToDefault
+            } #if
+
+        } #Try
+        catch {
+            $Err = $_
+            $ProgressPreference = $ProPref
+            throw $Err
+        } #catch
+
+    } #PROCESS
+
+    END {
         $ProgressPreference = $ProPref
-    } catch {
-        $Err = $_
-        throw $Err
-    }
-}
+    } #END
+
+} #function New-VIGlobalPermission
